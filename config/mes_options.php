b7024c5d01a72b7596ffa7f22a13c6a6fd6ecc09
Rainer Müller <rainer@websimple.be>
2013-05-03 13:58:57 +0200

    forcer lang

---
+++ config/mes_options.php
@@ -0,0 +1,13 @@
+<?php
+$GLOBALS['dossier_squelettes']='sites/websimple.be/squelettes';
+$GLOBALS['toujours_paragrapher']=true;
+$GLOBALS['forcer_lang']=true;
+$GLOBALS['derniere_modif_invalide']=true;
+if (!defined('_DUREE_CACHE_DEFAUT')) define('_DUREE_CACHE_DEFAUT',86400);
+if (!defined('_DELAI_CACHE_resultats')) define('_DELAI_CACHE_resultats',600);
+$GLOBALS['quota_cache']=100;
+if (!defined('_LOGIN_TROP_COURT')) define('_LOGIN_TROP_COURT',4);
+if (!defined('_TRANCHES')) define('_TRANCHES',10);
+$GLOBALS['filtrer_javascript']=1;
+
+?>
